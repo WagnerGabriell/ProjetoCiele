7429f881c3252a5f9239554335650695d8f36773
0220482112029 <0220482112029@LAB1MAQ27>
2022-05-13 13:31:40 -0300

    Add project files.

---
+++ ProjetoCiele/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoCiele.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
